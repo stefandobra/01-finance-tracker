a1ba090872a412e1fa4209045ecb15fbea080157
Stefan Dobra <dobrastefan@gmail.com>
2026-04-11 22:13:00 +0100

    Add spending options 3 and 4, improve summary formatting and money left calculation

--- finance_tracker.py
+++ finance_tracker.py
@@ -1,4 +1,5 @@
 def display_menu():
+    print("\n --- Budget Tracker ---")
     print("1. Add regular income")
     print("2. Add one-off income")
     print("3. Add regular spending")
@@ -14,14 +15,11 @@ def monthly(frequency, amount):
     else:
         return amount
 
-
 regular_incomes = []
 oneoff_incomes = []
 
 regular_spendings = []
-oneoff_spending = []
-
-
+oneoff_spendings = []
 
 while True:
     display_menu()
@@ -33,14 +31,14 @@ while True:
         income_description = input("Describe income: ")
         income_amount = float(input("Please enter income amount: "))
         while True:
-            frequency = input("How often do you receive this income?\n1. Daily\n2. Weekly\n3. Monthly\nPlease enter frequency: ") 
+            frequency = input("How often do you receive this income?\n1. Daily\n2. Weekly\n3. Monthly\nPlease enter frequency: ")
             if frequency not in ("1","2","3"):
                 print("Invalid Selection!")
             else:
                 break
-            
+
         monthly_income = monthly(frequency, income_amount)
-        
+
         regular_income = {
             "category" : income_category, 
             "income_description" : income_description, 
@@ -49,7 +47,7 @@ while True:
         regular_incomes.append(regular_income)
 
     elif option == "2":
-        income_category = input("What category is this regular income? ")
+        income_category = input("What category is this one off income? ")
         income_description = input("Describe income: ")
         income_amount = float(input("Please enter income amount: "))
 
@@ -60,18 +58,82 @@ while True:
             }
         oneoff_incomes.append(oneoff_income)
 
+    elif option == "3":
+        spending_category = input("What category is this regular spending? ")
+        spending_description = input("Describe spending: ")
+        spending_amount = float(input("Please enter spending amount: "))
+
+        while True:
+            frequency = input("How often do you pay this?\n1. Daily\n2. Weekly\n3. Monthly\nPlease enter frequency: ")
+            if frequency not in ("1","2","3"):
+                print("Invalid Selection!")
+            else:
+                break
+        
+        monthly_spending = monthly(frequency, spending_amount)
+
+        regular_spending = {
+            "category" : spending_category, 
+            "spending_description" : spending_description, 
+            "monthly_spending" : monthly_spending
+        }        
+
+        regular_spendings.append(regular_spending)
+
+    elif option == "4":
+        spending_category = input("What category is this one off spending? ")
+        spending_description = input("Describe spending: ")
+        spending_amount = float(input("Please enter spending amount: "))
+
+        oneoff_spending = {
+            "category" : spending_category, 
+            "spending_description" : spending_description, 
+            "spending_amount" : spending_amount
+        }
+
+        oneoff_spendings.append(oneoff_spending)
+
     elif option == "5":
         print("Regular incomes: ", len(regular_incomes))
 
-    
         regular_income_total = 0
 
         for income in regular_incomes:
             regular_income_total += income["monthly_income"]
 
-        print("Monthly regular income: ", regular_income_total)
+        print(f"Monthly regular income: {regular_income_total:,.2f}")
+
+        oneoff_income_total = 0
+
+        for income in oneoff_incomes:
+            oneoff_income_total += income["income_amount"]
+
+        print("Total oneoff income: ", oneoff_income_total)
+
+        print("Total regular and oneoff income: ", regular_income_total + oneoff_income_total)
+
+        print("Regular spendings: ", len(regular_spendings))
+
+        regular_spending_total = 0
+        oneoff_spending_total = 0
+
+        for spending in regular_spendings:
+            regular_spending_total += spending["monthly_spending"]
+
+        print(f"Monthly regular spending: {regular_spending_total:,.2f}")
+
+        for spending in oneoff_spendings:
+            oneoff_spending_total += spending["spending_amount"]
+        
+        print("Total oneoff spending: ", oneoff_spending_total)
+        print("Total regular and oneoff spending: ", regular_spending_total + oneoff_spending_total)
+
+        print(f"Monthly money left after regular spending: {(regular_income_total-regular_spending_total):,.2f}")
+        print(f"Monthly money left after total spending {(regular_income_total-(regular_spending_total + oneoff_spending_total)):,.2f}")
+
 
     elif option == "6":
+        print("Quitting program....")
         break
 
     else:
